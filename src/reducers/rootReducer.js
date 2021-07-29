270bfdce0f2269bb188ea12866cfc44d1df6c890
MrWhiteX <bartlomiej.bankosz@gmail.com>
2021-07-29 16:45:09 +0200

    redux persist

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,15 @@
 import { combineReducers } from "redux";
 import { appReducer } from "./appReducer";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export const rootReducer = combineReducers({
+const persistConfig = {
+  key: "todos",
+  storage,
+};
+
+const rootReducer = combineReducers({
   content: appReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,8 @@
 import { createStore } from "redux";
-import { rootReducer } from "../reducers/rootReducer";
+import rootReducer from "../reducers/rootReducer";
+import { persistStore } from "redux-persist";
 
 const store = createStore(rootReducer);
 
-export default store;
+const persistor = persistStore(store);
+export { store, persistor };
